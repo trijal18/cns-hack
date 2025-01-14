e6cbf96d25c4094e5e162ffdf914c3d1275179ba
Trijal <123671129+trijal18@users.noreply.github.com>
2025-01-15 02:56:29 +0530

    encryption

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,40 @@
-from fastapi import FastAPI
-import json
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
-import apimodels
-from models import temp
+from apimodels import UrlRequest
 from qcs.bin_to_pkl import bin_to_pkl
+from models import temp
 import os
- 
+import uuid
+
 app = FastAPI()
- 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
 
 @app.post("/fishy/")
-async def check_fishy(reqBody: apimodels.UrlRequest):
-    url=reqBody.url
-    res=temp.check_fishy(url)
-    jsonResponse = JSONResponse(status_code=200, content=json.loads(str(res)))
-    return jsonResponse
+async def check_fishy(reqBody: UrlRequest):
+    try:
+        url = reqBody.url
+        res = temp.check_fishy(url)
+        return JSONResponse(status_code=200, content=res)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error checking fishy: {str(e)}")
 
 @app.post("/check_url/")
-async def check_url(reqBody: apimodels.UrlRequest):
-    bin_to_pkl()
-    url=reqBody.url
-    res=rf.process_url(url)
-    os.remove("models/decrypted_randomForest.pkl")
-    jsonResponse = JSONResponse(status_code=200, content=res)
-    return jsonResponse
-    
+async def check_url(reqBody: UrlRequest):
+    try:
+        decrypted_model_path = f"models/decrypted_randomForest.pkl"
+
+        bin_to_pkl()  
+
+        url = reqBody.url
+        res = rf.process_url(url, model_path=decrypted_model_path)  
+
+        if os.path.exists(decrypted_model_path):
+            os.remove(decrypted_model_path)
+
+        return JSONResponse(status_code=200, content=res)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing URL: {str(e)}")
